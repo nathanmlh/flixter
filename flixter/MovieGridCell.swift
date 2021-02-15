8483d716e9abbd7240cb3c3ac152720e7bee096c
nathanmlh <nathanholdom1@gmail.coomo>
2021-02-15 13:02:03 -0500

    Assignment 2 for codepath. Added movie details page and tab that has only superhero movies.

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Nathan Holdom on 2/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
